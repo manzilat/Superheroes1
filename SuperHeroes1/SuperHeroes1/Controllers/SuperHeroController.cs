fe31ea41d1609bf10a3b01630fbe99e1bea1537f
manzilat <amtulnoorq@yahoo.com>
2018-12-04 15:44:05 -0600

    created a controller and added some information to index action and added details action

--- SuperHeroes1/SuperHeroes1/Controllers/SuperHeroController.cs
+++ SuperHeroes1/SuperHeroes1/Controllers/SuperHeroController.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using SuperHeroes1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SuperHeroes1.Controllers
+{
+    public class SuperHeroController : Controller
+    {
+        public ApplicationDbContext db = new ApplicationDbContext();
+        // GET: SuperHero
+        public ActionResult Index()
+        {
+            return View(db.SuperHero.ToList());
+        }
+
+        // GET: SuperHero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+    }
+
+}
