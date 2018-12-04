d8b0ee78967b1ccd189a514ba893a0c3177c5b74
manzilat <amtulnoorq@yahoo.com>
2018-12-04 15:41:03 -0600

    created a superheroes model

---
+++ SuperHeroes1/SuperHeroes1/Models/SuperHero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes1.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int SuperHeroID { get; set; }
+        [Display(Name = "Hero Name")]
+        public string SuperHeroName { get; set; }
+        [Display(Name = "Alter Ego")]
+        public string AlterEgo { get; set; }
+        [Display(Name = "Primary SuperHero Ability")]
+        public string PrimarySuperHeroAbility { get; set; }
+        [Display(Name = "Secondary SuperHero Ability")]
+        public string SecondarySuperHeroAbility { get; set; }
+        [Display(Name = "Catch phrase")]
+        public string Catchphrase { get; set; }
+    }
+}
