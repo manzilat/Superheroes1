6beb00b3fd268462e89d4457cabfa01ee9c54c4b
manzilat <amtulnoorq@yahoo.com>
2018-12-05 16:26:36 -0600

    added views for create and made edit action

--- SuperHeroes1/SuperHeroes1/Controllers/SuperHeroController.cs
+++ SuperHeroes1/SuperHeroes1/Controllers/SuperHeroController.cs
@@ -21,6 +21,51 @@ namespace SuperHeroes1.Controllers
         {
             return View();
         }
+        // GET: SuperHero/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperHero/Create
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "SuperHeroName,AlterEgo,PrimarySuperHeroAbility,SecondarySuperHeroAbility,CatchPhrase")] SuperHero superHero)
+        {
+
+            db.SuperHero.Add(superHero);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        // GET: SuperHero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            SuperHero superHero = db.SuperHero.Find(id);
+            return View(superHero);
+        }
+
+        // POST: SuperHero/Edit/5
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "SuperHeroName,AlterEgo,PrimarySuperHeroAbility,SecondarySuperHeroAbility,CatchPhrase")] SuperHero superHero, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                SuperHero updatedSuperHero = db.SuperHero.Find(id);
+                if (updatedSuperHero == null)
+                {
+                    return RedirectToAction("DisplayError", "SuperHero");
+                }
+                updatedSuperHero.SuperHeroName = superHero.SuperHeroName;
+                updatedSuperHero.AlterEgo = superHero.AlterEgo;
+                updatedSuperHero.PrimarySuperHeroAbility = superHero.PrimarySuperHeroAbility;
+                updatedSuperHero.SecondarySuperHeroAbility = superHero.SecondarySuperHeroAbility;
+                updatedSuperHero.Catchphrase = superHero.Catchphrase;
+                db.Entry(updatedSuperHero).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(superHero);
+        }
 
     }
 
